566496da0c21432bf64c1db01645f9075ae82bc7
Jean Borges <jeanborges946@gmail.com>
2023-03-02 15:53:47 -0300

    feature: Estruturação inicial da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import './global.css';
+
+function App() {
+  return (
+    <>
+      <div className={styles.wrapper}>Hello World</div>
+    </>
+  );
+}
+
+export default App;
